cf860ea40323f6709dafa3de1803fb3d0aa8d648
Mingjie Jiang <jiang@mingjie.dev>
2021-05-15 23:42:32 -0400

    Fix callback sequence

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -14,13 +14,22 @@ const authConfig = {
   secret: config.auth0.secret,
   baseURL: config.baseUrl,
   clientID: config.auth0.client,
-  issuerBaseURL: config.auth0.domain
+  issuerBaseURL: config.auth0.domain,
+  routes: {
+    login: false
+  }
 }
 
 /** attach /logout, /callback */
 router.use(auth(authConfig))
 
-router.get('/callback', async (req, res) => {
+router.get('/login', (req, res) => {
+  return res.oidc.login({
+    returnTo: '/gateway'
+  })
+})
+
+router.get('/gateway', async (req, res) => {
   await db.query(
     'INSERT INTO users (user_id, email, avatar, last_synced) VALUES ($1, $2, $3, NOW()) \
                   ON CONFLICT (user_id) DO UPDATE \
@@ -34,6 +43,8 @@ router.get('/callback', async (req, res) => {
 /* pass all user context to rendering */
 const passAuthContext = async (req, res, next) => {
   // context is optional
+  console.log('context passed @ ' + req.id)
+
   if (req.oidc.isAuthenticated()) {
     // pass context
     const adminCheck = await db.query('SELECT admin FROM users WHERE user_id = $1', [
@@ -41,7 +52,10 @@ const passAuthContext = async (req, res, next) => {
     ])
 
     res.locals.user = req.oidc.user
-    res.locals.user.admin = adminCheck.rows[0].admin
+    
+    if (adminCheck.rows.length > 0) {
+      res.locals.user.admin = adminCheck.rows[0].admin
+    }
   }
 
   next()
