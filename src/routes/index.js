25d72a677f32ad8a4966da1b222e9e2991615e00
CF12 <brianx0011@gmail.com>
2021-05-16 15:47:20 -0400

    Simulate network req for dash page

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,7 +13,39 @@ router.use(auth.passAuthContext)
 router.get('/', (req, res) => {
   if (req.oidc.isAuthenticated()) {
     return res.render('pages/dash', {
-      title: 'Dashboard'
+      title: 'Dashboard',
+      // Temporary test object to simulate network req
+      data: {
+        projects: [
+          {
+            title: '“Simon Says”',
+            description:
+              'This is a hardware task. You’ll be getting a hardware kit in the mail, and you’ll be expected to complete the design.',
+            type: 'Learning Task',
+            thumbnailUrl: '<WIP | url to image>',
+            numModulesRequired: 3,
+            numModulesAvailable: 6
+          },
+          {
+            title: '“Ship Wreck”',
+            description:
+              'This is a data science task. You’ll be getting a set of data to analyze, and you’ll be expected to present your results.',
+            type: 'Learning Task',
+            thumbnailUrl: '<WIP | url to image>',
+            numModulesRequired: 2,
+            numModulesAvailable: 3
+          },
+          {
+            title: '“Justice Matrix”',
+            description:
+              'This is an ethics in tech task. You’ll be given specifications and asked to design an algorithm to solve a problem ethically.',
+            type: 'Learning Task',
+            thumbnailUrl: '<WIP | url to image>',
+            numModulesRequired: 2,
+            numModulesAvailable: 8
+          }
+        ]
+      }
     })
   } else {
     return res.render('pages/landing', {
