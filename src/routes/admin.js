a04777b72bd007f1d1c1b09213e1064f6e85ec9b
Mingjie Jiang <jiang@mingjie.dev>
2021-05-18 02:49:29 -0400

    Fix set header after send

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -11,7 +11,7 @@ const db = require('@db')
 router.use((req, res, next) => {
   if (!res.locals.user.admin) {
     req.flash('error', 'You do not have permission to access this page.')
-    res.redirect('/')
+    return res.redirect('/')
   }
   res.locals.admin_zone = true
   next()
