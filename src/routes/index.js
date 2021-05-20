f9a43ff897cfceb9e1048244f199d116b821a7da
CF12 <brianx0011@gmail.com>
2021-05-19 21:40:57 -0400

    Add module page

--- src/routes/index.js
+++ src/routes/index.js
@@ -58,60 +58,85 @@ router.get('/', (req, res) => {
         // Temporary test object to simulate network req
         data: {
           project: {
-              title: '“Simon Says”',
-              description:
-                'This is a hardware task. You’ll be getting a hardware kit in the mail, and you’ll be expected to complete the design.',
-              type: 'Learning Task',
-              thumbnailUrl: '<WIP | url to image>',
-              numModulesRequired: 3,
-              numModulesAvailable: 6
+            title: '“Simon Says”',
+            description:
+              'This is a hardware task. You’ll be getting a hardware kit in the mail, and you’ll be expected to complete the design.',
+            type: 'Learning Task',
+            thumbnailUrl: '<WIP | url to image>',
+            numModulesRequired: 3,
+            numModulesAvailable: 6
           },
           modulesRequired: [
             {
+              id: '53a78ae7-ff40-41d4-8523-6fbf02fe62d4',
               title: 'Blinking Lights',
-              description: 'Create a ring of blinking lights using iterative loops & Adafruit MakeCode.',
+              description:
+                'Create a ring of blinking lights using iterative loops & Adafruit MakeCode.',
               points: 1
             },
             {
+              id: '53a78ae7-ff40-41d4-8523-6fbf02fe62d4',
               title: 'Blinking Lights',
-              description: 'Create a ring of blinking lights using iterative loops & Adafruit MakeCode.',
+              description:
+                'Create a ring of blinking lights using iterative loops & Adafruit MakeCode.',
               points: 1
             },
             {
+              id: '53a78ae7-ff40-41d4-8523-6fbf02fe62d4',
               title: 'Blinking Lights',
-              description: 'Create a ring of blinking lights using iterative loops & Adafruit MakeCode.',
+              description:
+                'Create a ring of blinking lights using iterative loops & Adafruit MakeCode.',
               points: 1
-            },
+            }
           ],
           modulesOptional: [
             {
+              id: '53a78ae7-ff40-41d4-8523-6fbf02fe62d4',
               title: 'Blinking Lights',
-              description: 'Create a ring of blinking lights using iterative loops & Adafruit MakeCode.',
+              description:
+                'Create a ring of blinking lights using iterative loops & Adafruit MakeCode.',
               points: 1
             },
             {
+              id: '53a78ae7-ff40-41d4-8523-6fbf02fe62d4',
               title: 'Blinking Lights',
-              description: 'Create a ring of blinking lights using iterative loops & Adafruit MakeCode.',
+              description:
+                'Create a ring of blinking lights using iterative loops & Adafruit MakeCode.',
               points: 1
             },
             {
+              id: '53a78ae7-ff40-41d4-8523-6fbf02fe62d4',
               title: 'Blinking Lights',
-              description: 'Create a ring of blinking lights using iterative loops & Adafruit MakeCode.',
+              description:
+                'Create a ring of blinking lights using iterative loops & Adafruit MakeCode.',
               points: 1
             },
             {
+              id: '53a78ae7-ff40-41d4-8523-6fbf02fe62d4',
               title: 'Blinking Lights',
-              description: 'Create a ring of blinking lights using iterative loops & Adafruit MakeCode.',
+              description:
+                'Create a ring of blinking lights using iterative loops & Adafruit MakeCode.',
               points: 1
             },
             {
+              id: '53a78ae7-ff40-41d4-8523-6fbf02fe62d4',
               title: 'Blinking Lights',
-              description: 'Create a ring of blinking lights using iterative loops & Adafruit MakeCode.',
+              description:
+                'Create a ring of blinking lights using iterative loops & Adafruit MakeCode.',
               points: 1
             },
             {
+              id: '53a78ae7-ff40-41d4-8523-6fbf02fe62d4',
               title: 'Blinking Lights',
-              description: 'Create a ring of blinking lights using iterative loops & Adafruit MakeCode.',
+              description:
+                'Create a ring of blinking lights using iterative loops & Adafruit MakeCode.',
+              points: 1
+            },
+            {
+              id: '53a78ae7-ff40-41d4-8523-6fbf02fe62d4',
+              title: 'Blinking Lights',
+              description:
+                'Create a ring of blinking lights using iterative loops & Adafruit MakeCode.',
               points: 1
             }
           ]
@@ -126,6 +151,7 @@ router.get('/', (req, res) => {
 })
 router.use('/debug', require('@routes/debug'))
 router.use('/admin', require('@routes/admin'))
+router.use('/module', require('@routes/module'))
 
 // TODO: Error pages should go here.
 
---
+++ src/routes/module.js
@@ -0,0 +1,41 @@
+/**
+    Module page routing & rendering
+*/
+
+const router = require('express').Router()
+
+// Represents an array of module objects
+let modules = [
+  {
+    id: '53a78ae7-ff40-41d4-8523-6fbf02fe62d4', // UUIDv4
+    title: 'Blinking Lights',
+    thumbnailUrl: '<WIP | url to image>',
+    description: 'Create a ring of blinking lights using iterative loops & Adafruit MakeCode.',
+    // HTML body content
+    content: `
+<h2>Module Introduction</h2>
+<p>Ever played with the old-school toy “Simon Says”? Well, you’ll be building that this week! You’ll be receiving an Adafruit Circuit Playground Express kit in the mail, and you’ll be building the full game with the board!</p>
+<h2>Basic Requirements</h2>
+<ul>
+  <li>Utilize at least 2 forms of outputs and 2 different types of sensors</li>
+  <li>Make a functional game!</li>
+  <li>Make it playable!</li>
+</ul>
+`,
+    points: 1
+  }
+]
+
+router.get('/:uuid', (req, res, next) => {
+  // Simulate db query for module based on id
+  const module = modules.find((e) => e.id === req.params.uuid)
+
+  if (!module) next()
+
+  res.render('pages/module', {
+    title: module.title,
+    data: module
+  })
+})
+
+module.exports = router
