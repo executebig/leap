e3db8e5edec22733d8f1af7f87ce60b082fb2279
Mingjie Jiang <jiang@mingjie.dev>
2021-05-23 18:44:07 -0400

    General housekeeping and layout update

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -1,7 +1,39 @@
 const router = require('express').Router()
 
+const UserController = require('@controllers/user.controllers')
+
+router.get('/', (req, res) => {
+    
+})
+
 router.get('/onboard', (req, res) => {
+  // verify user tag correct
+  if (req.user.state !== 'onboarding') {
+    req.flash('error', 'You have already completed onboarding!')
+    return res.redirect('/dash')
+  }
+
   res.render('pages/account/onboard', { title: 'Welcome' })
+})
+
+/* updates the req.user object */
+router.get('/refresh', async (req, res) => {
+  const user = await UserController.getUserById(req.user.user_id)
+
+  req.login(user, (err) => {
+    if (err) {
+      res.status(500).send({
+        success: false,
+        error: err,
+        user
+      })
+    } else {
+      res.send({
+        success: true,
+        user
+      })
+    }
+  })
 })
 
 module.exports = router
