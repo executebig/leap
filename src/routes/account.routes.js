dcc05edf67b253064d3644f74c0c3de781a872a9
Mingjie Jiang <jiang@mingjie.dev>
2021-05-23 21:44:20 -0400

    Update onboarding form

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -2,9 +2,7 @@ const router = require('express').Router()
 
 const UserController = require('@controllers/user.controllers')
 
-router.get('/', (req, res) => {
-    
-})
+router.get('/', (req, res) => {})
 
 router.get('/onboard', (req, res) => {
   // verify user tag correct
@@ -16,24 +14,41 @@ router.get('/onboard', (req, res) => {
   res.render('pages/account/onboard', { title: 'Welcome' })
 })
 
+router.post('/onboard', async (req, res) => {
+  let data = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    display_name: req.body.display_name,
+    age: parseInt(req.body.age),
+    parent_email: req.body.parent_email,
+    no_shipping: req.body.no_shipping === 'on',
+    address:
+      req.body.no_shipping === 'on'
+        ? null
+        : `${req.body.addr_street_1}, ${req.body.addr_street_2}, ${req.body.addr_city}, ${req.body.addr_state}`,
+    phone: req.body.phone,
+    state: 'idle'
+  }
+
+  const user = await UserController.updateUser(req.user.user_id, data)
+  refreshUser(req, res, user)
+})
+
 /* updates the req.user object */
 router.get('/refresh', async (req, res) => {
   const user = await UserController.getUserById(req.user.user_id)
+  refreshUser(req, res, user)
+})
 
+const refreshUser = (req, res, user) => {
   req.login(user, (err) => {
     if (err) {
-      res.status(500).send({
-        success: false,
-        error: err,
-        user
-      })
+      req.flash('error', err.message)
+      res.redirect('/dash')
     } else {
-      res.send({
-        success: true,
-        user
-      })
+      res.redirect('/dash')
     }
   })
-})
+}
 
 module.exports = router
