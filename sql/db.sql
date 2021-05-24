cc21e01a51fda3d4487d4e3c9e393d2fe910d185
Mingjie Jiang <jiang@mingjie.dev>
2021-05-23 21:47:36 -0400

    Update db schema to match onboarding

--- sql/db.sql
+++ sql/db.sql
@@ -1,10 +1,20 @@
+create type user_state as enum ('onboarding', 'inprogress', 'completed', 'idle');
+
 create table if not exists users (
     user_id serial not null constraint users_pk primary key,
     email text not null,
     created_at timestamp not null,
     updated_at timestamp not null,
     admin boolean default false,
-    state user_state default 'onboarding' :: user_state not null
+    state user_state default 'onboarding' :: user_state not null,
+    display_name text,
+    first_name text,
+    last_name text,
+    age integer,
+    no_shipping boolean default false not null,
+    address text,
+    phone text,
+    parent_email text
 );
 
 create unique index if not exists users_user_id_uindex_2 on users (user_id);
