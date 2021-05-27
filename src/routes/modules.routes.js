009722c52c1553d09a2b1922057351177b20d092
CF12 <brianx0011@gmail.com>
2021-05-26 21:44:40 -0400

    Fix styling for /modules + implement db query for /modules/:id

--- src/routes/modules.routes.js
+++ src/routes/modules.routes.js
@@ -6,24 +6,22 @@
 const router = require('express').Router()
 
 const ProjectController = require('@controllers/project.controllers')
+const ModuleController = require('@controllers/module.controllers')
 
 router.get('/', async (req, res) => {
   return res.render('pages/modules', {
     title: 'Modules',
+    week: req.user.current_week,
     // TODO: Use user selected project id
     data: await ProjectController.getProjectAndModulesById(1)
   })
 })
 
 // TODO: Implement db queries for module & authorized access
-router.get('/:uuid', (req, res, next) => {
-  // Simulate db query for module based on id
-  const module = debugModules.find((e) => e.id === req.params.uuid)
+router.get('/:id', async (req, res, next) => {
+  const module = await ModuleController.getModule(req.params.id, 1)
 
-  // Simulate additional auth check (ensure user specifically has access to module)
-  const authorized = true
-
-  if (!module || !authorized) next()
+  if (!module) return next()
 
   res.render('pages/module', {
     title: module.title,
