084abf2f42b6b3b4b287fa3f5e0fa558a5a4de88
CF12 <brianx0011@gmail.com>
2021-06-09 18:48:46 -0400

    Swap flag & state middlewares

--- src/routes/protected.routes.js
+++ src/routes/protected.routes.js
@@ -10,8 +10,8 @@ const { flagMiddleware, stateMiddleware } = require('@middlewares/state.middlewa
 /** Admin route handled separately (to hide from users) */
 router.use('/admin', optionalAuth, flagMiddleware, require('@routes/admin.routes'))
 
-router.use('/dash', stateMiddleware, flagMiddleware, require('@routes/dash.routes'))
-router.use('/account', stateMiddleware, flagMiddleware, require('@routes/account.routes'))
-router.use('/modules', stateMiddleware, flagMiddleware, require('@routes/modules.routes'))
+router.use('/dash', flagMiddleware, stateMiddleware, require('@routes/dash.routes'))
+router.use('/account', flagMiddleware, stateMiddleware, require('@routes/account.routes'))
+router.use('/modules', flagMiddleware, stateMiddleware, require('@routes/modules.routes'))
 
 module.exports = router
