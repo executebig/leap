6a2a795a235f68d530999c52080f7ab6ae3ff904
CF12 <brianx0011@gmail.com>
2021-06-09 20:58:25 -0400

    Fix /admin stalling when not logged in

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -6,25 +6,23 @@
 const router = require('express').Router()
 
 const { flagMiddleware, banMiddleware } = require('@middlewares/state.middlewares')
-const { optionalAuth } = require('@middlewares/auth.middlewares')
 
 const db = require('@db')
-
-// Check for flag sessions + disallow if banned
-router.use(optionalAuth, flagMiddleware, banMiddleware)
 
 /** allow admins only */
 router.use((req, res, next) => {
   if (!req.user || !req.user.admin) {
     res.redirect('/404')
   } else {
-    res.locals.layout = 'admin'
     next()
   }
 })
 
+// Check for flag sessions + disallow if banned
+router.use(flagMiddleware, banMiddleware)
+
 router.get('/', (req, res) => {
-  res.render('pages/admin/dashboard')
+  res.render('pages/admin/dashboard', { layout: 'admin' })
 })
 
 module.exports = router
