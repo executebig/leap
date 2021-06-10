5078afd46b82a136f3572910dda46b194fa9ba7b
CF12 <brianx0011@gmail.com>
2021-06-09 21:22:37 -0400

    Add onboarding user tests

--- test/test-user.js
+++ test/test-user.js
@@ -6,6 +6,7 @@
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const suppressLogs = require('mocha-suppress-logs')
+const crypto = require('crypto')
 
 const app = require('../src/server')
 
@@ -61,6 +62,82 @@ describe('Test unauthorized user functions', () => {
     return chai
       .request(app)
       .get('/admin')
+      .then((res) => {
+        res.should.have.status(404)
+      })
+      .catch((err) => {
+        throw err
+      })
+  })
+
+  it(`404 on random page`, () => {
+    return chai
+      .request(app)
+      .get(`/${crypto.randomBytes(16).toString('hex')}`)
+      .then((res) => {
+        res.should.have.status(404)
+      })
+      .catch((err) => {
+        throw err
+      })
+  })
+})
+
+describe('Test onboarding user functions', () => {
+  suppressLogs()
+
+  let cookies
+
+  it(`Login via /debug/chai/login`, () => {
+    return chai
+      .request(app)
+      .get('/debug/chai/login')
+      .then((res) => {
+        res.should.have.status(200)
+        res.text.should.equal('success')
+
+        cookies = res.headers['set-cookie'].pop().split(';')[0]
+      })
+      .catch((err) => {
+        throw err
+      })
+  })
+
+  it(`302 on /dash -> /account/onboard`, () => {
+    return chai
+      .request(app)
+      .get('/dash')
+      .set('Cookie', cookies)
+      .redirects(0)
+      .then((res) => {
+        res.should.have.status(302)
+        res.should.redirectTo('/account/onboard')
+      })
+      .catch((err) => {
+        throw err
+      })
+  })
+
+  it(`302 on /modules -> /dash`, () => {
+    return chai
+      .request(app)
+      .get('/modules')
+      .set('Cookie', cookies)
+      .redirects(0)
+      .then((res) => {
+        res.should.have.status(302)
+        res.should.redirectTo('/dash')
+      })
+      .catch((err) => {
+        throw err
+      })
+  })
+
+  it(`404 on /admin`, () => {
+    return chai
+      .request(app)
+      .get('/admin')
+      .set('Cookie', cookies)
       .then((res) => {
         res.should.have.status(404)
       })
