baa61cbec0c291ac9faf41397064663b2042890b
CF12 <brianx0011@gmail.com>
2021-06-09 21:34:51 -0400

    Add pending user tests

--- test/test-user.js
+++ test/test-user.js
@@ -43,7 +43,7 @@ describe('Test unauthorized user functions', () => {
 
   let paths = ['/dash', '/modules', '/account']
   paths.forEach((path) => {
-    it(`302 on ${path}`, () => {
+    it(`302 on ${path} -> /`, () => {
       return chai
         .request(app)
         .get(path)
@@ -83,10 +83,10 @@ describe('Test unauthorized user functions', () => {
   })
 })
 
+let cookies
+
 describe('Test onboarding user functions', () => {
   suppressLogs()
-
-  let cookies
 
   it(`Login via /debug/chai/login`, () => {
     return chai
@@ -146,3 +146,97 @@ describe('Test onboarding user functions', () => {
       })
   })
 })
+
+describe('Test pending user functions', () => {
+  suppressLogs()
+
+  it(`301 POST /account/onboard -> /account/invite`, () => {
+    return chai
+      .request(app)
+      .post('/account/onboard')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .send('first_name=a&last_name=a&display_name=42&age=42&parent_email=&no_shipping=true&addr_street_1=&addr_street_2=&addr_city=&addr_state=&addr_zip=&phone=1231231234')
+      .set('Cookie', cookies)
+      .redirects(0)
+      .then((res) => {
+        res.should.have.status(302)
+        res.should.redirectTo('/account/invite')
+      })
+      .catch((err) => {
+        throw err
+      })
+  })
+
+  it(`200 on /dash`, () => {
+    return chai
+      .request(app)
+      .get('/dash')
+      .set('Cookie', cookies)
+      .then((res) => {
+        res.should.have.status(200)
+      })
+      .catch((err) => {
+        throw err
+      })
+  })
+
+  it(`302 on /modules -> /dash`, () => {
+    return chai
+      .request(app)
+      .get('/modules')
+      .set('Cookie', cookies)
+      .redirects(0)
+      .then((res) => {
+        res.should.have.status(302)
+        res.should.redirectTo('/dash')
+      })
+      .catch((err) => {
+        throw err
+      })
+  })
+
+  it(`302 on /modules/0 -> /dash`, () => {
+    return chai
+      .request(app)
+      .get(`/modules/0`)
+      .set('Cookie', cookies)
+      .redirects(0)
+      .then((res) => {
+        res.should.have.status(302)
+        res.should.redirectTo('/dash')
+      })
+      .catch((err) => {
+        throw err
+      })
+  })
+
+  it(`302 on /modules/:random_id -> /dash`, () => {
+    return chai
+      .request(app)
+      .get(`/modules/${Math.floor(Math.random() * 500) + 1000}`)
+      .set('Cookie', cookies)
+      .redirects(0)
+      .then((res) => {
+        res.should.have.status(302)
+        res.should.redirectTo('/dash')
+      })
+      .catch((err) => {
+        throw err
+      })
+  })
+
+  it(`404 on /admin`, () => {
+    return chai
+      .request(app)
+      .get('/admin')
+      .set('Cookie', cookies)
+      .then((res) => {
+        res.should.have.status(404)
+      })
+      .catch((err) => {
+        throw err
+      })
+  })
+})
+
+// TODO: inprogress user tests
