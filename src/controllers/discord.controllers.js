ea2ef4602075598c584eae324a4c7ac5ed4b278d
Mingjie Jiang <jiang@mingjie.dev>
2021-06-27 15:31:25 -0400

    Chill page updates

--- src/controllers/discord.controllers.js
+++ src/controllers/discord.controllers.js
@@ -10,8 +10,7 @@ client.on('ready', () => {
   client.user.setPresence({
     activity: {
       type: 'PLAYING',
-      name: 'techroulette.xyz',
-      url: 'techroulette.xyz',
+      name: 'Tech Roulette'
     },
     status: 'online'
   })
