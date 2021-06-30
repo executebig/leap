5db9cdb3115983571221c092ca6a7f9a9588a53c
CF12 <brianx0011@gmail.com>
2021-06-30 16:35:32 -0400

    Add submissions page

--- src/routes/submissions.routes.js
+++ src/routes/submissions.routes.js
@@ -17,7 +17,7 @@ const reflash = require('@libs/reflash')
 // Check for session flag, user banned, & state updates
 router.use(checkAuth, flagMiddleware, banMiddleware, stateMiddleware)
 
-router.get('/:projectId', async (req, res) => {
+router.use('/:projectId', async (req, res, next) => {
   if (isNaN(req.params.projectId)) {
     notFoundMiddleware(req, res)
     return
@@ -26,21 +26,48 @@ router.get('/:projectId', async (req, res) => {
   const projectId = parseInt(req.params.projectId, 10)
 
   // Ensure user has permission to view project
-  if (
-    !req.user.prev_projects.includes(projectId) &&
-    req.user.current_project !== projectId
-  ) {
+  if (!req.user.prev_projects.includes(projectId) && req.user.current_project !== projectId) {
     notFoundMiddleware(req, res)
     return
   }
 
+  next()
+})
+
+router.get('/:projectId', async (req, res) => {
+  const projectId = parseInt(req.params.projectId, 10)
+
   const project = await ProjectController.getProjectById(projectId)
-  const submissions = await SubmissionController.getSubmissionsByUserAndProjectId(req.user.user_id, projectId)
+  const submissions = await SubmissionController.getSubmissionsByUserAndProjectId(
+    req.user.user_id,
+    projectId
+  )
 
   return res.render('pages/submissions', {
     project,
     submissions
   })
+})
+
+router.get('/:projectId/:submissionId', async (req, res) => {
+  if (isNaN(req.params.submissionId)) {
+    notFoundMiddleware(req, res)
+    return
+  }
+
+  const submission = await SubmissionController.getSubmission(
+    req.user.user_id,
+    req.params.projectId,
+    req.params.submissionId
+  )
+
+  if (!submission) {
+    res.status(404)
+    res.json({ error: 'Not Found' })
+    return
+  }
+
+  res.json({ submission })
 })
 
 module.exports = router
